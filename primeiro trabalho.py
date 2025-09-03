a17ef7cbf1526fe795d435d148542cb4a287af91
pedrobenedetmachado <pedrohbm0102@gmail.com>
2025-09-03 20:28:27 -0300

    projeto simples sobre variaveis

---
+++ primeiro trabalho.py
@@ -0,0 +1,8 @@
+nome=input('qual seu nome?:')
+peso=input('qual seu peso?:')
+idade=input('qual sua idade?:')
+print(f'olá {nome} prazer em te conhecer!')
+print(f'você tem {idade} anos e {peso} quilos')
+
+
+
